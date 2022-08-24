072bba527a2c1c3b91fef6e32f81abe87c8fcc25
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-24 23:35:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(e){
+    let text = document.querySelector('#text');
+    text.textContent = "This is really cool!"; 
+    console.log("mbj")
+});
+
+//let text = document.querySelector('#text');
+//text.textContent = "This is really cool!";
